d517ee0979ee5112ebcfc26be6bd7f3c2bccc47a
= <=>
2017-12-11 11:16:50 +0530

    created basic trends layout

--- src/Components/SubComponents/trends.js
+++ src/Components/SubComponents/trends.js
@@ -7,11 +7,104 @@ const styles ={};
 class Trends extends Component{
     render(){
         return(
-            <Grid container style={{height:"50vh",backgroundColor:"#ffffff"}}>
+            <Grid container style={{backgroundColor:"#ffffff"}} direction="column">
                 <Grid item>
-                    <Typography type="title">
-                        Trends Grid
-                    </Typography>
+                    <Grid container>
+                        <Grid item>
+                            <Typography type="title">
+                                Trends Grid
+                            </Typography>
+                        </Grid>
+                        <Grid item style={{display:"flex",alignItems:"flex-end",paddingLeft:0}}>
+                            <Typography type="caption">
+                                Change
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item>
+                    <Grid container direction="column">
+                        <Grid item style={{paddingBottom:0}}>
+                            <Typography type="body2" style={{color:"rgb(29, 161, 242)",fontWeight:600}}>
+                                #MondayMotivation
+                            </Typography>
+                        </Grid>
+                        <Grid item style={{paddingTop:0}}>
+                            <Typography type="caption">
+                                @yrf, @ranveerbrar and 1 more are Tweeting about this
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item>
+                    <Grid container direction="column">
+                        <Grid item style={{paddingBottom:0}}>
+                            <Typography type="body2" style={{color:"rgb(29, 161, 242)",fontWeight:600}}>
+                                #MondayMotivation
+                            </Typography>
+                        </Grid>
+                        <Grid item style={{paddingTop:0}}>
+                            <Typography type="caption">
+                                @yrf, @ranveerbrar and 1 more are Tweeting about this
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item>
+                    <Grid container direction="column">
+                        <Grid item style={{paddingBottom:0}}>
+                            <Typography type="body2" style={{color:"rgb(29, 161, 242)",fontWeight:600}}>
+                                #MondayMotivation
+                            </Typography>
+                        </Grid>
+                        <Grid item style={{paddingTop:0}}>
+                            <Typography type="caption">
+                                @yrf, @ranveerbrar and 1 more are Tweeting about this
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item>
+                    <Grid container direction="column">
+                        <Grid item style={{paddingBottom:0}}>
+                            <Typography type="body2" style={{color:"rgb(29, 161, 242)",fontWeight:600}}>
+                                #MondayMotivation
+                            </Typography>
+                        </Grid>
+                        <Grid item style={{paddingTop:0}}>
+                            <Typography type="caption">
+                                @yrf, @ranveerbrar and 1 more are Tweeting about this
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item>
+                    <Grid container direction="column">
+                        <Grid item style={{paddingBottom:0}}>
+                            <Typography type="body2" style={{color:"rgb(29, 161, 242)",fontWeight:600}}>
+                                #MondayMotivation
+                            </Typography>
+                        </Grid>
+                        <Grid item style={{paddingTop:0}}>
+                            <Typography type="caption">
+                                @yrf, @ranveerbrar and 1 more are Tweeting about this
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item>
+                    <Grid container direction="column">
+                        <Grid item style={{paddingBottom:0}}>
+                            <Typography type="body2" style={{color:"rgb(29, 161, 242)",fontWeight:600}}>
+                                #MondayMotivation
+                            </Typography>
+                        </Grid>
+                        <Grid item style={{paddingTop:0}}>
+                            <Typography type="caption">
+                                @yrf, @ranveerbrar and 1 more are Tweeting about this
+                            </Typography>
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
         )
